95a2e499b9fa06d398cf82439d50ba885cccbf5c
Sacha Sedriks <sachased@gmail.com>
2020-02-18 09:13:50 -0800

    update html/css/js

--- unit2-1-4/script.js
+++ unit2-1-4/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// vvvvv see footer re: $(document).ready...
+
+function init() {
+  $('#important-events h1').addClass('js-important');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FOOTER
+// to make sure that you don't execute any script before the document is fully loaded, start with a document ready function — which tells the code to wait to execute until the document is ready to load — and wraps all of our code inside it
+
+$(document).ready(function() {
+  console.log("javascript ready!");
+  init();
+});
