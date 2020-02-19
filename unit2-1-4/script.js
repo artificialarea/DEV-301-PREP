0638fc9f7db8f81578f183d8382e71617b3f9bcf
Sacha Sedriks <sachased@gmail.com>
2020-02-19 10:50:35 -0800

    various jquery experiments

--- unit2-1-4/script.js
+++ unit2-1-4/script.js
@@ -3,28 +3,26 @@
 // vvvvv see footer re: $(document).ready...
 
 function init() {
+  
+  // hides by embedding in HTML => style="display:none;"
+  $('#unimportant-events').hide();
+
+  // add class to HTML element, styling handled by style.css
   $('#important-events h1').addClass('js-important');
 
+  // note .css method, embeds css style directly into HTML
+  $('li:even').css('background-color', 'yellow');
+  $('li:first-child').css('background-color', 'red');
+
+  // add properties to numbers e.g. <p><span>9</span>
+  // note object syntax to allow for multiple properties to be defined
+  $('p span').css({
+    'background-color': 'orange',
+    'color': 'red',
+    'font-size': '100px'
+  });
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
