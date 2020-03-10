54566adfe3f6e1115341646e627db68569e2721e
Sacha Sedriks <sachased@gmail.com>
2020-03-10 11:41:54 -0700

    thumbnail carousel functionality complete. next up: hero image

--- PREP-v4/unit3-1-3/cat_carousel/index.js
+++ PREP-v4/unit3-1-3/cat_carousel/index.js
@@ -1,7 +1,31 @@
 'use strict';
 
-$('.thumbnail').on('click', e => {
-  console.log(e.target);
+$('.js-thumbnail').on('click', e => {
+  // save reference to selected cat image
+  const targetCat = $(e.currentTarget);
+  // and reference all the other images (for inactive toggle purposes)
+  const otherCats = $('.js-thumbnail').not(targetCat);
+
+  // store whether this image is aria-pressed.
+  // because .attr() returns strings, we convert it
+  // back to a boolean by making a strict operative comparison.
+  const pressedBool = $(targetCat).attr('aria-pressed') === 'true';
+
+  // remove 'selected-thumbnail' and uncheck cats that are not this one
+  // note chained attributes of .removeClass() + .attr() 
+  otherCats.removeClass('selected-thumbnail').attr('aria-pressed', false);
+
+  // toggle the presence of 'selected-cat'
+  // tpggle aria-pressed by inverting boolean (using the ! opertator)
+  // conditional to ensure it doesn't toggle if already selected
+  if (!pressedBool) { 
+    targetCat.addClass('selected-thumbnail').attr('aria-pressed', !pressedBool);
+  }
+
+  // // let input = $('input:image').css( {border: '3px solid red'});
+  // let input = $(e.currentTarget).css( {border: '3px solid red'});
+
+
 });
 
 // PSEUDOCODE
@@ -9,6 +33,7 @@ $('.thumbnail').on('click', e => {
 // when user selects one of the thumbnail images
 // using a mouse 
 // OR keyboard (TAB key to one of thumbnails, select with ENTER key)
+// (Note to self: change html so ARIA-friendly, e.g. change <img> to <input type="image">, toggle with aria-selected=true/false attribute, image-on/image-off class, etc)
 
 
 // [1] that image should be displayed in full-sized image container on the top
