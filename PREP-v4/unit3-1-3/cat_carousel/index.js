c61725638b48139d5d5699fa97e4f5147543301a
Sacha Sedriks <sachased@gmail.com>
2020-03-10 11:59:18 -0700

    update .css/.js. Functionality complete. Could still improve styling, tho.

--- PREP-v4/unit3-1-3/cat_carousel/index.js
+++ PREP-v4/unit3-1-3/cat_carousel/index.js
@@ -1,42 +1,41 @@
 'use strict';
 
-$('.js-thumbnail').on('click', e => {
-  // save reference to selected cat image
-  const targetCat = $(e.currentTarget);
-  // and reference all the other images (for inactive toggle purposes)
-  const otherCats = $('.js-thumbnail').not(targetCat);
-
-  // store whether this image is aria-pressed.
-  // because .attr() returns strings, we convert it
-  // back to a boolean by making a strict operative comparison.
-  const pressedBool = $(targetCat).attr('aria-pressed') === 'true';
-
-  // remove 'selected-thumbnail' and uncheck cats that are not this one
-  // note chained attributes of .removeClass() + .attr() 
-  otherCats.removeClass('selected-thumbnail').attr('aria-pressed', false);
-
-  // toggle the presence of 'selected-cat'
-  // tpggle aria-pressed by inverting boolean (using the ! opertator)
-  // conditional to ensure it doesn't toggle if already selected
-  if (!pressedBool) { 
-    targetCat.addClass('selected-thumbnail').attr('aria-pressed', !pressedBool);
-  }
-
-  // // let input = $('input:image').css( {border: '3px solid red'});
-  // let input = $(e.currentTarget).css( {border: '3px solid red'});
-
-
-});
-
 // PSEUDOCODE
 
 // when user selects one of the thumbnail images
-// using a mouse 
-// OR keyboard (TAB key to one of thumbnails, select with ENTER key)
-// (Note to self: change html so ARIA-friendly, e.g. change <img> to <input type="image">, toggle with aria-selected=true/false attribute, image-on/image-off class, etc)
+// using a mouse OR (ARIA-friendly) keyboard:
+// [1] that image should be displayed in full-sized (hero) image container on the top and [2] should update the `alt` attribute.
 
+// (Note to self: change html so ARIA-friendly, e.g. change <img> to <input type="image">, toggle with aria-selected=true/false attribute, image-on/image-off class, etc).
 
-// [1] that image should be displayed in full-sized image container on the top
+function handleCarousel() {
 
-// [2] should update the `alt` attribute
+  $('.js-thumbnail').on('click', e => {
+    
+    // save reference to selected cat image
+    const targetCat = $(e.currentTarget);
+    // and reference all the other images (for inactive toggle purposes)
+    const otherCats = $('.js-thumbnail').not(targetCat);
+    
+    // store whether this image is aria-pressed.
+    // because .attr() returns strings, we convert it
+    // back to a boolean by making a strict operative comparison.
+    const pressedBool = $(targetCat).attr('aria-pressed') === 'true';
+    
+    // remove 'selected-thumbnail' and uncheck cats that are not this one
+    // note chained attributes of .removeClass() + .attr() 
+    otherCats.removeClass('selected-thumbnail').attr('aria-pressed', false);
+    
+    // toggle the presence of 'selected-cat'
+    // tpggle aria-pressed by inverting boolean (using the ! opertator)
+    // conditional to ensure it doesn't toggle if already selected
+    // update hero image (an alt text) with selected thumbnail image 
+    if (!pressedBool) { 
+      targetCat.addClass('selected-thumbnail').attr('aria-pressed', !pressedBool);
+      $('.js-hero img').attr('src', targetCat.attr('src'));
+      $('.js-hero img').attr('alt', targetCat.attr('alt'));
+    }
+  });
+}
 
+$(handleCarousel);
