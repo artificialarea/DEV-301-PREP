78ed296226c300df192c869d037eb7cc1306aa15
Sacha Sedriks <sachased@gmail.com>
2020-03-10 16:18:15 -0700

    completed fizz-buzz exercise in my own way.

--- PREP-v4/unit3-1-3/fizz_buzz/index.js
+++ PREP-v4/unit3-1-3/fizz_buzz/index.js
@@ -1,2 +1,37 @@
 'use strict';
 
+function handleFizzBuzz() {
+
+  $('#number-chooser').on('submit', event => {
+    event.preventDefault(); // prevents submission posting
+    
+    const numChoice = $(event.currentTarget).find('#number-choice');
+    const num = numChoice.val();
+    let result;
+
+    // logic of fizzbuzz
+    if (num % 3 === 0 && num % 5 === 0) {
+      result = 'fizzbuzz';
+    } else if (num % 3 === 0) {
+      result = 'fizz';
+    } else if (num % 5 === 0) {
+      result = 'buzz';
+    } else {
+      result = num;
+    }
+
+    console.log(event.target);
+    console.log(num);
+    console.log(result);
+
+    $('.js-results').html(function() {
+      if (result !== num) {
+        return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
+      } else {
+        return `<div class='fizz-buzz-item'><span>${result}</span></div>`;
+      }
+    });
+  });
+}
+
+$(handleFizzBuzz);
