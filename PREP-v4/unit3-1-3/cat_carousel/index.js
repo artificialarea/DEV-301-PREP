183264ea14b04111f1bb9d79860cb5357963acbd
Sacha Sedriks <sachased@gmail.com>
2020-03-10 10:30:45 -0700

    update .css so styled appropriately. pseudocode .js.

--- PREP-v4/unit3-1-3/cat_carousel/index.js
+++ PREP-v4/unit3-1-3/cat_carousel/index.js
@@ -3,3 +3,15 @@
 $('.thumbnail').on('click', e => {
   console.log(e.target);
 });
+
+// PSEUDOCODE
+
+// when user selects one of the thumbnail images
+// using a mouse 
+// OR keyboard (TAB key to one of thumbnails, select with ENTER key)
+
+
+// [1] that image should be displayed in full-sized image container on the top
+
+// [2] should update the `alt` attribute
+
