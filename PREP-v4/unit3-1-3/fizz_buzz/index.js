51bc8778c91710c542a727b371db547f810ae4b7
Sacha Sedriks <sachased@gmail.com>
2020-03-11 11:55:08 -0700

    alternate version including an array that renders <div>s for each sequential number instance in the countdown

--- PREP-v4/unit3-1-3/fizz_buzz/index.js
+++ PREP-v4/unit3-1-3/fizz_buzz/index.js
@@ -5,32 +5,33 @@ function handleFizzBuzz() {
   $('#number-chooser').on('submit', event => {
     // prevent `form` from posting submission, resulting in error
     event.preventDefault(); 
-    
-    const numChoice = $(event.currentTarget).find('#number-choice');
-    const num = numChoice.val();
-    let result;
-
-    if (num % 3 === 0 && num % 5 === 0) {
-      result = 'fizzbuzz';
-    } else if (num % 3 === 0) {
-      result = 'fizz';
-    } else if (num % 5 === 0) {
-      result = 'buzz';
-    } else {
-      result = num;
-    }
-
-    // console.log(event.target);
-    // console.log(num);
-    // console.log(result);
-
-    $('.js-results').html(function() {
-      if (result !== num) {
-        return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
+    // get value submitted
+    const num = $(event.currentTarget).find('#number-choice').val();
+    // declare an array in anticipation of storing individual DOM elements for each number counted
+    const results = [];
+    // counting (and storing/pushing)
+    for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        results.push('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+        // ????
+        // NOTE: in repl.it solution they wrapped the items pushed in an additional jquery function results.push( $('...') );
+        // Dunno why. E.g. 
+        // results.push($(
+        //       '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));
+        // ???? 
+      } else if (i % 3 === 0) {
+        results.push('<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+      } else if (i % 5 === 0) {
+        results.push('<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
       } else {
-        return `<div class='fizz-buzz-item'><span>${result}</span></div>`;
+        results.push(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
       }
-    });
+    }
+    
+    // in case there are already results displayed
+    $('.js-results').empty();
+    // attach our results to the <div> element in the DOM
+    $('.js-results').append(results);
   });
 }
 
