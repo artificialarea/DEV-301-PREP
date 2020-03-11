9cdf9a5283d605b34d5b9c1d4408325019c3e712
Sacha Sedriks <sachased@gmail.com>
2020-03-11 10:35:01 -0700

    Revert "made a mess of .js, committing so I can revert back to previous SHA via git checkout."
    This reverts commit 2601950ccfa9b7c842153f22ae0b1c61c7609406.

--- PREP-v4/unit3-1-3/fizz_buzz/index.js
+++ PREP-v4/unit3-1-3/fizz_buzz/index.js
@@ -1,52 +1,35 @@
 'use strict';
 
-// so VSCode Console nodeJS doesn't have 
-// ReferenceError: $ is not defined
-// window.$ = window.jQuery;
-
 function handleFizzBuzz() {
 
   $('#number-chooser').on('submit', event => {
-    // prevent `form` submission behavior, resulting in error
+    // prevent `form` from posting submission, resulting in error
     event.preventDefault(); 
     
-    
+    const numChoice = $(event.currentTarget).find('#number-choice');
+    const num = numChoice.val();
+    let result;
+
+    if (num % 3 === 0 && num % 5 === 0) {
+      result = 'fizzbuzz';
+    } else if (num % 3 === 0) {
+      result = 'fizz';
+    } else if (num % 5 === 0) {
+      result = 'buzz';
+    } else {
+      result = num;
+    }
+
+    // console.log(event.target);
+    // console.log(num);
+    // console.log(result);
 
     $('.js-results').html(function() {
-      // purging submission from texfield
-      const numChoice = $(event.currentTarget).find('#number-choice');
-      const num = numChoice.val();
-      let result;
-
-      console.log(event.target);
-      console.log(num);
-      
-
-      for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-          result = 'fizzbuzz';
-          return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
-        } else if (i % 3 === 0) {
-          result = 'fizz';
-          return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
-        } else if (i % 5 === 0) {
-          result = 'buzz';
-          return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
-        } else {
-          result = i;
-          return `<div class='fizz-buzz-item'><span>${result}</span></div>`;
-        }
-        console.log(result);
-      }
-
-      // if just wanted a *single* result
-      /*
       if (result !== num) {
         return `<div class='fizz-buzz-item ${result}'><span>${result}</span></div>`;
       } else {
         return `<div class='fizz-buzz-item'><span>${result}</span></div>`;
       }
-      */
     });
   });
 }
